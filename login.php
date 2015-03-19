a1cdf50de3dbe1e1f80bd37967e811ea9071155f
jamesferry84 <jrferry@gmail.com>
2015-03-19 15:24:02 +0000

    enhancements made to mobile views

--- login.php
+++ login.php
@@ -45,114 +45,104 @@ include 'init.php';
         </div>
     </div>
     <div class="container">
-        <div class="alert-danger"><?php echo $_GET["$error"];  ?></div>
-    <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-        <div class="panel panel-success" >
-            <div class="panel-heading">
-                <div class="panel-title">Sign In</div>
-            </div>
+        <div id="loginbox" style="margin-top:50px;" class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+            <div class="panel panel-success" >
+                <div class="panel-heading">
+                    <div class="panel-title">Sign In</div>
+                </div>
 
-            <div style="padding-top:30px" class="panel-body" >
+                <div class="panel-body" >
+                    <form id="loginform" class="form-horizontal" action="checklogin.php" method="post">
 
-                <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
-
-                <form id="loginform" class="form-horizontal" action="checklogin.php" method="post">
-
-                    <div style="margin-bottom: 25px" class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                        <input id="login-username" type="email" class="form-control" name="username" value="" placeholder="email">
-                    </div>
-
-                    <div style="margin-bottom: 25px" class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                        <input id="login-password" type="password" class="form-control" name="password" placeholder="password">
-                    </div>
-
-                    <div style="margin-top:10px" class="form-group">
-                        <div class="col-sm-12 controls">
-                            <input type="submit" class="btn btn-success" name="Submit" value="Login">
-                        </div>
-                    </div>
-
-
-                    <div class="form-group">
-                        <div class="col-md-12 control">
-                            <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
-                                Don't have an account!
-                                <a href="#" onClick="$('#loginbox').hide(); $('#signupbox').show()">
-                                    Sign Up Here
-                                </a>
+                        <div class="form-group" style="margin-top:20px;">
+                            <label for="username" class="col-md-3 control-label">Email:</label>
+                            <div class="col-md-9">
+                                <input id="login-username" type="email" class="form-control" name="username" value="" placeholder="email" required="true">
                             </div>
                         </div>
-                    </div>
-                </form>
+
+                        <div class="form-group">
+                            <label for="password" class="col-md-3 control-label">password:</label>
+                            <div class="col-md-9">
+                                <input id="login-password" type="password" class="form-control" name="password" placeholder="password" required="true">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-offset-3 col-md-9">
+                                <input type="submit" class="btn btn-success" name="Submit" value="Login">
+                            </div>
+                        </div>
 
 
+                        <div class="form-group">
+                            <div class="col-md-12 control">
+                                <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
+                                    Don't have an account!
+                                    <a href="#" onClick="$('#loginbox').hide(); $('#signupbox').show()">
+                                        Sign Up Here
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div id="signupbox" style="display:none; margin-top:50px" class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <div class="panel-title">Sign Up</div>
+                    <div style="float:right; font-size: 85%; position: relative; top:-10px"><a id="signinlink" href="#" onclick="$('#signupbox').hide(); $('#loginbox').show()">Sign In</a></div>
+                </div>
+                <div class="panel-body" >
+                    <form id="signupform" class="form-horizontal" action="register.php" method="post">
 
+                        <div class="form-group">
+                            <label for="email" class="col-md-3 control-label">Email</label>
+                            <div class="col-md-9">
+                                <input type="email" class="form-control" name="email" placeholder="Email Address" required="true">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password" class="col-md-3 control-label">Password</label>
+                            <div class="col-md-9">
+                                <input type="password" class="form-control" name="password" placeholder="Password" required="true">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="firstname" class="col-md-3 control-label">First Name</label>
+                            <div class="col-md-9">
+                                <input type="text" class="form-control" name="firstname" placeholder="First Name" required="true">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="lastname" class="col-md-3 control-label">Last Name</label>
+                            <div class="col-md-9">
+                                <input type="text" class="form-control" name="lastname" placeholder="Last Name" required="true">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="teamname" class="col-md-3 control-label">Team Name</label>
+                            <div class="col-md-9">
+                                <input type="text" class="form-control" name="teamname" placeholder="Team Name" required="true">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <!-- Button -->
+                            <div class="col-md-offset-3 col-md-9">
+                                <input type="submit" class="btn btn-success" name="Submit" value="Sign Up">
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-    <div id="signupbox" style="display:none; margin-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-        <div class="panel panel-success">
-            <div class="panel-heading">
-                <div class="panel-title">Sign Up</div>
-                <div style="float:right; font-size: 85%; position: relative; top:-10px"><a id="signinlink" href="#" onclick="$('#signupbox').hide(); $('#loginbox').show()">Sign In</a></div>
-            </div>
-            <div class="panel-body" >
-                <form id="signupform" class="form-horizontal" action="register.php" method="post">
-
-                    <div id="signupalert" style="display:none" class="alert alert-danger">
-                        <p>Error:</p>
-                        <span></span>
-                    </div>
-
-
-
-                    <div class="form-group">
-                        <label for="email" class="col-md-3 control-label">Email</label>
-                        <div class="col-md-9">
-                            <input type="email" class="form-control" name="email" placeholder="Email Address" required="true">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="password" class="col-md-3 control-label">Password</label>
-                        <div class="col-md-9">
-                            <input type="password" class="form-control" name="password" placeholder="Password" required="true">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="firstname" class="col-md-3 control-label">First Name</label>
-                        <div class="col-md-9">
-                            <input type="text" class="form-control" name="firstname" placeholder="First Name" required="true">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="lastname" class="col-md-3 control-label">Last Name</label>
-                        <div class="col-md-9">
-                            <input type="text" class="form-control" name="lastname" placeholder="Last Name" required="true">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="teamname" class="col-md-3 control-label">Team Name</label>
-                        <div class="col-md-9">
-                            <input type="text" class="form-control" name="teamname" placeholder="Team Name" required="true">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <!-- Button -->
-                        <div class="col-md-offset-3 col-md-9">
-                            <input type="submit" class="btn btn-success" name="Submit" value="Sign Up">
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-
-
-
-
-    </div>
-</div>
 </body>
 </html>
--- submit.php
+++ submit.php
@@ -45,30 +45,32 @@ $conn->query($query);
 <div class="container">
     <div class=" col-lg-12 col-md-12 col-sm-12">
         <h5 class="alert-success">Thank You. Your selections have been accepted and are displayed below.</h5>
-        <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>Race</th>
-                <th>Driver 1</th>
-                <th>Driver 2</th>
-                <th>Constructor 1</th>
-                <th>Constructor 2</th>
-                <th>Joker Used</th>
-                <th>New Balance</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td><?php echo $country; ?></td>
-                <td><?php echo $driver1; ?></td>
-                <td><?php echo $driver2; ?></td>
-                <td><?php echo $constructor1; ?></td>
-                <td><?php echo $constructor2; ?></td>
-                <td><?php echo $joker; ?></td>
-                <td><?php echo '£' . number_format($balance,2,'.',''); ?></td>
-            </tr>
-        </tbody>
-        </table>
+        <div class="table-responsive ">
+            <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Race</th>
+                    <th>Driver 1</th>
+                    <th>Driver 2</th>
+                    <th>Constructor 1</th>
+                    <th>Constructor 2</th>
+                    <th>Joker Used</th>
+                    <th>New Balance</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><?php echo $country; ?></td>
+                    <td><?php echo $driver1; ?></td>
+                    <td><?php echo $driver2; ?></td>
+                    <td><?php echo $constructor1; ?></td>
+                    <td><?php echo $constructor2; ?></td>
+                    <td><?php echo $joker; ?></td>
+                    <td><?php echo '£' . number_format($balance,2,'.',''); ?></td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
     </div>
 </div>
 <?php
