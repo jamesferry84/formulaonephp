5cc22294d9c231d39e24543f8327e075c579c0f2
jamesferry84 <jrferry@gmail.com>
2015-03-21 22:37:30 +0000

    admin changes

--- admin.php
+++ admin.php
@@ -61,6 +61,71 @@ else{
             </div>
         </div>
     </div>
+
+    <div class = "row">
+        <div class = "col-lg-12 col-md-12 col-sm-12">
+            <div class = "panel panel-default">
+                <div class = "panel-body">
+                    <div class="page-header">
+                        <?php
+                            $sql = "select * from racecalendar where Date >= CURDATE() LIMIT 0,1";
+                            $queryResult = $conn->query($sql);
+                            $numrows=mysqli_num_rows($queryResult);
+
+                            while($row = mysqli_fetch_assoc($queryResult))
+                            {
+                            $country =  $row["Country"];
+                            }
+                        ?>
+                        <h3>Submissions for -  <?php echo $country ?> </h3>
+                    </div>
+                    <div class="table-responsive ">
+                            <table class="table table-striped">
+                                <thead>
+                                <tr>
+                                    <th>Team</th>
+                                    <th>Driver 1</th>
+                                    <th>Driver 2</th>
+                                    <th>Constructor 1</th>
+                                    <th>Constructor 2</th>
+
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <?php
+                                    $sql = "select * from racecalendar where Date >= CURDATE() LIMIT 0,1";
+                                    $queryResult = $conn->query($sql);
+                                    $numrows=mysqli_num_rows($queryResult);
+
+                                    while($row = mysqli_fetch_assoc($queryResult))
+                                    {
+                                        $country =  $row["Country"];
+                                    }
+
+//                                    $query = "SELECT u.username, email, teamname FROM `users` u join `submissions` s on u.username = s.username  AND s.country = '$country'";
+                                    $query = "SELECT teamname, driver1, driver2, constructor1,constructor2 FROM `submissions` s join `users` u on u.username = s.username  AND s.country = '$country'";
+                                    $usersNotSubmitted = $conn->query($query);
+                                    while($row = mysqli_fetch_assoc($usersNotSubmitted))
+                                    {
+                                        echo'<tr>' .
+                                            '<td>' . $row["teamname"] . '</td>' .
+                                            '<td>' . $row["driver1"] . '</td>' .
+                                            '<td>' . $row["driver2"] . '</td>' .
+                                            '<td>' . $row["constructor1"] . '</td>' .
+                                            '<td>' . $row["constructor2"] . '</td>' .
+                                            '</tr>';
+                                    }
+                                    ?>
+                                </tr>
+                                </tbody>
+
+                            </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php
