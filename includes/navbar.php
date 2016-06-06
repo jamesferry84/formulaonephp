c155d8c0fae9d9b93504bd6de3c1b49a4e6617af
jamesferry84 <jrferry@gmail.com>
2016-06-06 20:15:13 +0100

    fixed typo on <li> tag

--- includes/navbar.php
+++ includes/navbar.php
@@ -16,17 +16,18 @@ include 'init.php';
 
         <div class="collapse navbar-collapse navHeaderCollapse">
             <ul class = "nav navbar-nav navbar-right">
-                <li<?php if ($active == 'home') {echo 'class="active"';} ?>><a href="index.php">Home</a></li>
-                <li<?php if ($active == 'rules') {echo 'class="active"';} ?>><a href="rules.php">Rules</a></li>
-                <li<?php if ($active == 'profile') {echo 'class="active"';} ?>><a href="profile.php">Profile</a></li>
-                <?php
-                if ($_SESSION["admin"] == 1)
-                {
-                    echo "<li><a href='admin.php'>Admin</a></li>";
-                }
-                ?>
-
-                <li><a href="logout.php">Log Out</a></li>
+                <li <?php if ($active == 'home') {echo 'class="active"';} ?>>
+                    <a href="index.php">Home</a>
+                </li>
+                <li <?php if ($active == 'rules') {echo 'class="active"';} ?>>
+                    <a href="rules.php">Rules</a>
+                </li>
+                <li <?php if ($active == 'profile') {echo 'class="active"';} ?>>
+                    <a href="profile.php">Profile</a>
+                </li>
+                <li>
+                    <a href="logout.php">Log Out</a>
+                </li>
             </ul>
         </div>
     </div>
