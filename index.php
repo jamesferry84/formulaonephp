73c965ee21a4d746361eb99f717aa8f5689a58ee
jamesferry84 <jrferry@gmail.com>
2016-06-07 20:40:56 +0100

    updated index page with table of overall standings

--- index.php
+++ index.php
@@ -44,6 +44,40 @@ include 'includes/navbar.php';
 
 <div class = "container">
     <div class="row">
+
+        <div class="table-responsive  col-lg-12">
+            <table class="table table-bordered text-center">
+                <tr class ="success">
+                    <td>+/-</td><td>Rank</td><td>Player Name</td><td>Team Name</td><td>Player Score</td><td>Jokers Played</td><td>Budget Rollover</td>
+                </tr>
+                <?php
+                $today = date("y-m-d");
+                $sql = "select * from users order by points desc LIMIT 0,3";
+                $queryResult = $conn->query($sql);
+                $numrows=mysqli_num_rows($queryResult);
+                $rank = 0;
+                while($row = mysqli_fetch_assoc($queryResult))
+                {
+                    $rank++;
+                    ?>
+                    <tr>
+                        <td></td>
+                        <td><?php echo $rank?></td>
+                        <td><?php echo $row["UserName"]?></td>
+                        <td><?php echo $row["teamname"]?></td>
+                        <td><?php echo $row["points"]?></td>
+                        <td> <?php echo $row["jokers"]?></td>
+                        <td> <?php echo $row["budget"]?></td>
+                    </tr>
+                    <?php
+                }
+                ?>
+            </table>
+        </div>
+
+
+
+
         <div class="col-md-6">
             <h3>Top 3</h3>
             <div class="table-responsive ">
