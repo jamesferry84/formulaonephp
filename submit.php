7e5164d87a2dba3eaa572dd0b4d9f442f98de57b
jamesferry84 <jrferry@gmail.com>
2016-06-07 20:54:30 +0100

    adding table of submissions to choose team page

--- submit.php
+++ submit.php
@@ -23,7 +23,7 @@ $active="home";
         }
         else {
             $today = date("y-m-d");
-            $sql = "select * from racecalendar where Date >= CURDATE() LIMIT 0,1";
+            $sql = "select * from racecalendar where Date >= CURDATE() order by date LIMIT 0,1";
             $queryResult = $conn->query($sql);
             $numrows=mysqli_num_rows($queryResult);
             $country = "";
