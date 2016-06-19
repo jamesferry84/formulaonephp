2a7997017e2ed38bf9304bf03f02e24f4abed396
jamesferry84 <jrferry@gmail.com>
2016-06-19 19:41:19 +0100

    cleared session properly on log out and redirected to login if user tries to go directly to page

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,13 @@
+<?php
+if (isset($_SESSION["username"]))
+{
+    // DO NOTHING
+}
+else{
+    header("location:login.php");
+}
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
--- logout.php
+++ logout.php
@@ -1,4 +1,10 @@
 <?php
+//unset($_SESSION['username']);
+//session_destroy();
+session_start();
+setcookie(session_name(), '', 100);
+session_unset();
 session_destroy();
+$_SESSION = array();
 header("location:login.php");
 
