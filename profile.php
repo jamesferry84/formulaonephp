7731dc94e4f565c43aae3f1234e45b4bdb2cf722
jamesferry84 <jrferry@gmail.com>
2016-06-25 21:31:33 +0100

    profile layout changed

--- profile.php
+++ profile.php
@@ -33,76 +33,83 @@ while($row = mysqli_fetch_assoc($result))
 
             ?>
         </div>
-        <div class = "row">
-            <div class = "col-lg-12 col-md-12 col-sm-12">
-                <div class = "panel panel-default">
-                    <div class = "panel-body">
-                        <div class="page-header">
-                            <h3>Profile</h3>
+        <div class = "col-lg-12 col-md-12 col-sm-12">
+            <div class = "panel panel-default">
+                <div class = "panel-body">
+                    <div class="page-header">
+                        <h3>Profile</h3>
+                    </div>
+
+                    <div id="emailBox" style="margin-top:50px;" class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+                        <div class="panel panel-success" >
+                            <div class="panel-heading">
+                                <div class="panel-title">Team Details</div>
+                            </div>
+
+                            <div class="panel-body" >
+                                <form id="emailUpdateForm" class="form-horizontal" action="updateEmail.php" method="post" onsubmit='return checkEmail()'>
+
+                                    <div class="form-group" style="margin-top:20px;">
+                                        <label for="teamname" class="col-md-3 control-label">Team Name:</label>
+                                        <div class="col-md-9">
+                                            <input id="login-username" type="email" class="form-control" name="teamname" placeholder="<?php echo $teamName?>" disabled="disabled"  >
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group" style="margin-top:20px;">
+                                        <label for="email" class="col-md-3 control-label">Email:</label>
+                                        <div class="col-md-9">
+                                            <input id="email" type="email" class="form-control" name="email" placeholder="<?php echo $emailAddress?>"   >
+                                        </div>
+                                    </div>
+
+
+                                    <div class="form-group">
+                                        <div class="col-md-offset-3 col-md-9">
+                                            <input type="submit" class="btn btn-success" name="updateEmail" value="Update Email">
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+
+                            <div class="panel-body" >
+                                <form id="passwordUpdateForm" class="form-horizontal" action="updatePAssword.php" method="post" onsubmit='return checkPassword()'>
+
+                                    <div class="form-group" style="margin-top:20px;">
+                                        <label for="currentPassword" class="col-md-3 control-label">Current Password:</label>
+                                        <div class="col-md-9">
+                                            <input id="currentPassword" type="password" class="form-control" name="currentPassword" required="required" >
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group" style="margin-top:20px;">
+                                        <label for="newPassword" class="col-md-3 control-label">New Password:</label>
+                                        <div class="col-md-9">
+                                            <input id="newPassword" type="password" class="form-control" name="newPassword" required="required" >
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group" style="margin-top:20px;">
+                                        <label for="confrimNewPassword" class="col-md-3 control-label">Confirm New Password:</label>
+                                        <div class="col-md-9">
+                                            <input id="confirmNewPassword" type="password" class="form-control" name="confirmNewPassword" required="required" >
+                                        </div>
+                                    </div>
+
+
+                                    <div class="form-group">
+                                        <div class="col-md-offset-3 col-md-9">
+                                            <input type="submit" class="btn btn-success" name="updatePassword" value="Update Password">
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
-                        <form class="form-horizontal" action="updateEmail.php" method="post" onsubmit='return checkEmail()'>
-
-                            <div class="table-responsive  col-lg-12 col-md-12 col-sm-12 ">
-                                <table class="table table-bordered text-center">
-                                    <tr>
-                                        <td class="success">Team Name:</td>
-                                        <td><?php echo $teamName?></td>
-                                    </tr>
-                                    <tr>
-                                        <td class="success">Email:</td>
-                                        <td> <input type="email" class="form-control" name="email" id="email" placeholder="<?php echo $emailAddress?>" required="required"></td>
-                                    </tr>
-                                    <tr>
-                                        <td></td>
-                                        <td></td>
-                                    </tr>
-                                </table>
-                                <div class="form-group">
-                                    <!-- Button -->
-                                    <div class="col-md-offset-9 col-sm-offset-9 col-lg-offset-9col-lg-9 col-md-9 col-sm-9">
-                                        <input type="submit" class="btn btn-success" name="Submit" value="Update Email">
-                                    </div>
-                                </div>
-
-                            </div>
-                        </form>
-
-                        <form class="form-horizontal" action="updatePassword.php" method="post" onsubmit='return checkPassword()'>
-                            <div class="table-responsive  col-lg-12 col-md-12 col-sm-12 ">
-                                <table class="table table-bordered text-center">
-                                    <tr>
-                                        <td class="success">Current Password:</td>
-                                        <td><input type="password" class="form-control" name="currentPassword" id="currentPassword" required="required"></td>
-                                    </tr>
-                                    <tr>
-                                        <td class="success">New Password:</td>
-                                        <td> <input type="password" class="form-control" name="newPassword" id="newPassword"  required="required"></td>
-                                    </tr>
-                                    <tr>
-                                        <td class="success">Confirm New Password:</td>
-                                        <td> <input type="password" class="form-control" name="confirmNewPassword" id="confirmNewPassword"  required="required"></td>
-                                    </tr>
-                                    <tr>
-                                        <td></td>
-                                        <td></td>
-                                    </tr>
-                                </table>
-                                <div class="form-group">
-                                    <!-- Button -->
-                                    <div class="col-md-offset-9 col-sm-offset-9 col-lg-offset-9col-lg-9 col-md-9 col-sm-9">
-                                        <input type="submit" class="btn btn-success" name="Submit" value="Update Password">
-                                    </div>
-                                </div>
-
-                            </div>
-                        </form>
-
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
     <script>
         function checkEmail() {
             var currentEmail = "<?php echo $emailAddress?>";
